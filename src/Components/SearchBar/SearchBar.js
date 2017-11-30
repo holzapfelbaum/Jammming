570b2ce58c9d696a7a1e5b47288b9dd283d20509
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-11-29 23:30:49 -0800

    Added a SearchBar component class and a render method that returns JSX for the SearchBar

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <a>SEARCH</a>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
