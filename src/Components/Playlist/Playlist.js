2e4c14b4d8c6aae4d868d6fcb1b864fa5c8b42f4
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-11-30 23:27:53 -0800

    Added tracks attribute that passes this.props.playlistTracks from Playlist component in App.js to TrackList in Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -10,7 +10,7 @@ class Playlist extends React.Component {
   render() {
     <div className="Playlist">
       <input defaultValue={'New Playlist'} />
-      // <TrackList />
+      <TrackList tracks={this.props.playlistTracks} />
       <a className="Playlist-save">SAVE TO SPOTIFY</a>
     </div>
   }
