f161d8917fa585ffe0e77cde189b51a8475b6f99
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-12-02 22:32:56 -0800

    Passed onRemove from TrackList component in Playlist.js to Track component in TrackList.js

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -11,7 +11,7 @@ class TrackList extends React.Component {
     return (
       <div className="TrackList">
         {this.props.tracks.map(track => {
-          return <Track key={track.id} onAdd={this.props.onAdd} />
+          return <Track key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} />
         })}
       </div>
     )
