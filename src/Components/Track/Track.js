7544174963fe7d44f34c2e409ec81b3b47afc8a9
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-12-02 23:00:08 -0800

    Bound this.removeTrack to current value of this and added on-click property to Track-action link element with value of this.removeTrack

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -5,6 +5,7 @@ class Track extends React.Component {
   constructor(props) {
     super(props);
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   renderAction() {
@@ -30,7 +31,7 @@ class Track extends React.Component {
           <h3>{this.props.track.name}</h3>
           <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <a className="Track-action" onClick={this.addTrack}>
+        <a className="Track-action" onClick={this.addTrack} onClick={this.removeTrack}>
           {this.renderAction}
         </a>
       </div>
