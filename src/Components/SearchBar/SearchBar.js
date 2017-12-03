a29467ed3977cfa2c225f6158fafa4763c667c45
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-12-03 00:48:53 -0800

    Created onChange attribute in input in render method and set equal to this.handleTermChange

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,16 +5,21 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   search() {
     this.props.onSearch(this.props.term);
   }
 
+  handleTermChange(event) {
+    this.setState({this.props.term: event.target.value})
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
         <a>SEARCH</a>
       </div>
     )
