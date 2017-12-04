12893daf38e5ea3e050157f1c0d2f96dc27f666d
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-12-03 20:47:49 -0800

    Added some updates to getAccessToken() method that included defining accessToken using accessTokenMatch, defining expiresIn using expiresInMatch, and defining accessUrl and using window.location to redirect users to accessUrl in case accessToken variable is empty and not in url

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -5,36 +5,24 @@ let accessToken = '';
 const Spotify = {
   getAccessToken() {
     if (accessToken) {
-      return new Promise(resolve =>
-        resolve(accessToken));
+      return accessToken;
     }
 
-    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/); // what does the ([^&]*)/ stand for?
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/); // what does the ([^&]*)/ stand for? per Elise, ^ means matches beginning of input, * means matches preceding expression 0 or more times, and so in this case, "any number of characters until a & is found"; i.e. if you have https://example.com/callback#access_token=NwAExz...BV3O2Tk&token_type=Bearer&expires_in=3600&state=123, then it is everything between access_token= and the first &
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
     if (accessTokenMatch && expiresInMatch) {
-      let expiresIn = 15;
-      window.setTimeout(() => accessToken = '', expiresIn * 60);
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
       return accessToken;
     } else {
-      window.location.assign(`https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`);
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl; // in line above, we set the accessUrl according to Spotify's requirements; now, we set the url the user will be sent to as that accessUrl using window.location
     }
-  }
+  },
+
+  
 };
 
 export default Spotify;
-
-getAccessToken() {
-    if (accessToken) {
-      return accessToken;
-    }
-
-   //
-    //const expiresInMatch =
-    if (accessTokenMatch && expiresInMatch) {
-      //your code here
-      return accessToken;
-    } else {
-     // your code here
-    }
-  },
