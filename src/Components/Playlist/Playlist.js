5c40f251540ee27d49988da1cb854fa08df5fc24
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-12-04 20:30:47 -0800

    Added import of TrackList and return in render() method

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Playlist.css';
-// import TrackList from '../TrackList/TrackList.js';
+import TrackList from '../TrackList/TrackList.js';
 
 class Playlist extends React.Component {
   constructor(props) {
@@ -13,11 +13,13 @@ class Playlist extends React.Component {
   }
 
   render() {
-    <div className="Playlist">
-      <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
-      <TrackList tracks={this.props.playlistTracks} isRemoval={true} onRemove={this.props.onRemove} />
-      <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
-    </div>
+    return (
+      <div className="Playlist">
+        <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
+        <TrackList tracks={this.props.playlistTracks} isRemoval={true} onRemove={this.props.onRemove} />
+        <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
+      </div>
+    )
   }
 }
 
