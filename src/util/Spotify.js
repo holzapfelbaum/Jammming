bfba1e3e377b0a62fa8346bd87d0fa1241653fd6
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-12-04 20:27:35 -0800

    Added syntax fixes per Elise

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -48,7 +48,7 @@ const Spotify = {
     if (!playlistname && !trackuris.length) {
       return;
     }
-    let accessToken = `${accessToken}`;
+    const accessToken = Spotify.getAccessToken();
     let headers = {
       'Authorization': `Bearer ${accessToken}`
     }
@@ -56,17 +56,16 @@ const Spotify = {
     return fetch(`https://api.spotify.com/v1/me`, {headers: headers}).then(response => {
       return response.json();
     }).then(jsonResponse => {
-      response.id = userId;
-    }).then(fetch (`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+      userId = jsonResponse.id;
+      return fetch (`https://api.spotify.com/v1/users/${userId}/playlists/`, {
       headers: headers,
       method: 'POST',
-      body: JSON.stringify({name: playlistname})
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      playlistId = response.id;
-    }))
-  }
-};
+      body: JSON.stringify({name: name})
+    }).then(response => response.json()
+  ).then(jsonResponse => {
+      const playlistId = jsonResponse.id;
+    })
+  })
+}
 
 export default Spotify;
