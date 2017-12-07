6cefba2750d4a991cfb84e3e8d6fdbed516d864f
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-12-06 20:42:36 -0800

    Added a term state set to an empty string (which will take in a term in the search bar) and passed term as an argument to the search method in SearchBar component

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,11 +4,14 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      term: ''
+    };
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
 
-  search() {
+  search(term) {
     this.props.onSearch(this.state.term);
   }
 
