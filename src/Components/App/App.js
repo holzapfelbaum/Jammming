0b4b1ab81008959601c507f33191d1c6adba4103
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-12-06 20:54:15 -0800

    Added a variable promise set to the Spotify.search() method and set the state of searchResults to the value resolved from that promise

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
   removeTrack(track) {
     let tracks = this.state.playlistTracks;
     if (tracks.indexOf(track) > -1) { // since -1 means track is not present, if indexOf is greater than -1, track will be required to be in array
-      tracks.filter(currentTrack => currentTrack != track.id);
+      tracks.filter(currentTrack => currentTrack != track.id); // warning: "Expected '!==' and instead saw '!='"
       this.setState({playlistTracks: tracks})
     }
   }
@@ -50,7 +50,9 @@ class App extends React.Component {
   }
 
   search(term) {
-    Spotify.search(term);
+    const promise = Spotify.search(term);
+    // Update the state of searchResults with the value resolved from Spotify.search()'s promise.
+    this.setState({searchResults: promise});
   }
 
   render() {
